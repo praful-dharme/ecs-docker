46f7006256a4a3c8a00180f6d59d67f50a85f4e9
praful-dharme <102966841+praful-dharme@users.noreply.github.com>
2026-06-02 12:41:45 +0530

    Update router.ts

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => res.json({ message: "Hello from Docker v3 🎉" }));
+router.get("/", (req, res) => {
+  res.json({ message: "Hello from Docker --devcity 🎉" });
+});
 
 router.get("/health", (req, res) => {
-  throw new Error("Internal Server Error");
-  res.status(200).json({ message: "Everything is good here 👀" });
+  res.status(200).json({
+    status: "healthy",
+    message: "Everything is good here 👀",
+  });
 });
 
 export default router;
